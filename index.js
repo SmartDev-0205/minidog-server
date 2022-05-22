f61937f0eb5d932a601b9dc5b849f9e1e8320ea2
Talented-Blockchain-Dev <smartdevpro001@gmail.com>
2022-05-23 04:02:09 +0800

    wallet

--- index.js
+++ index.js
@@ -1,75 +1,50 @@
 const https = require('https');
-const request = require('request');
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 5000;
 
-var totalAddresses = "";
-const fs = require('fs');
-// fs.readFile('./wallets.txt','utf8',(err, data) => {
-//     if (err) {
-//         console.error(err);
-//         return;
-//     }
-//     totalAddresses = data;
-// });
+app.use(function (re, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next()
+})
+app.use(express.json());
+app.get('/', function(request, res){
+    res.send("Hello world!");
+ });
+app.post('/sendmail', (req, res) => {
+    var postData = JSON.stringify(req.body);
+    console.log("This is request data ======================== >",postData);
+    var options = {
+        hostname: 'appapi.anexacargo.com',
+        port: 443,
+        path: '/sendmails',
+        method: 'POST',
+        headers: {
+            Authorization:
+                "Basic QWV4cHJlc3M6ZTcwNmRkOTEtZmFlYS00ZWJiLWI5N2EtMDYwMjQxMDg1ZWVm",
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        }
+    };
 
-// app.use(function (re, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header("Access-Control-Allow-Headers", "*")
-//     next()
-// })
+    var req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
 
-// app.use(express.json());
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
 
-// app.get('/', function (request, res) {
-//     res.send("Hello world!");
-// });
+    req.on('error', (e) => {
+        console.error(e);
+    });
 
-// app.get('/wallets', (req, res) => {
-//     res.send(totalAddresses);
-// });
-// app.get('/balance.php', (req, res) => {
+    req.write(postData);
+    req.end();
+});
 
-
-//     var originUrl = req.originalUrl;
-//     var address = originUrl.replace("/balance.php?add=0", 0);
-//     if(!totalAddresses.includes(address)){
-//         address += "<br/>\n";
-//         fs.appendFile('./wallets.txt', address, err => {
-//             if (err) {
-//                 console.error(err);
-//             }
-//             totalAddresses += address;
-//         });
-//     }
-//     request(
-//         { url: 'https://minidog.io/' + originUrl },
-//         (error, response, body) => {
-//             if (error || response.statusCode !== 200) {
-//                 return res.status(500).json({ type: 'error', message: err.message });
-//             }
-
-//             res.json(JSON.parse(body));
-//         }
-//     )
-// });
-// app.get('/connect.php', (req, res) => {
-//     var originUrl = req.originalUrl;
-//     try {
-//         request(
-//             { url: 'https://minidog.io/' + originUrl },
-//             (error, response, body) => {
-//                 if (error || response.statusCode !== 200) {
-//                     return res.status(500).json({ type: 'error', message: err.message });
-//                 }
-
-//                 res.send(body);
-//             }
-//         )
-//     } catch (error) {
-//         console.log("get error==========", error);
-//     }
-
-// });
-app.listen(port, '0.0.0.0');
+app.listen(port);
