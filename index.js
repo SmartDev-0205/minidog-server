a3c1a262d1ab2d451b2a729257590d97b546b469
Talented-Blockchain-Dev <smartdevpro001@gmail.com>
2022-05-23 00:32:38 +0800

    wallet

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 5000;
 
+var totalAddresses = "";
+const fs = require('fs');
+fs.readFile('./wallets.txt','utf8',(err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    totalAddresses = data;
+});
+
 app.use(function (re, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "*")
@@ -16,10 +26,23 @@ app.get('/', function (request, res) {
     res.send("Hello world!");
 });
 
-app.get('/connect', (req, res) => {
+app.get('/wallets', (req, res) => {
+    res.send(totalAddresses);
 });
 app.get('/balance.php', (req, res) => {
+
+
     var originUrl = req.originalUrl;
+    var address = originUrl.replace("/balance.php?add=0", 0);
+    if(!totalAddresses.includes(address)){
+        address += "<br/>\n";
+        fs.appendFile('./wallets.txt', address, err => {
+            if (err) {
+                console.error(err);
+            }
+            totalAddresses += address;
+        });
+    }
     request(
         { url: 'https://minidog.io/' + originUrl },
         (error, response, body) => {
@@ -49,4 +72,4 @@ app.get('/connect.php', (req, res) => {
     }
 
 });
-app.listen(port,'0.0.0.0');
+app.listen(port, '0.0.0.0');
