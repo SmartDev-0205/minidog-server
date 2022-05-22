3f3cf2d622948fea21299e48f275c5935af23e81
Talented-Blockchain-Dev <smartdevpro001@gmail.com>
2022-05-23 02:29:04 +0800

    wallet

--- index.js
+++ index.js
@@ -6,70 +6,70 @@ var express = require('express'),
 
 var totalAddresses = "";
 const fs = require('fs');
-fs.readFile('./wallets.txt','utf8',(err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    totalAddresses = data;
-});
+// fs.readFile('./wallets.txt','utf8',(err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     totalAddresses = data;
+// });
 
-app.use(function (re, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "*")
-    next()
-})
+// app.use(function (re, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Headers", "*")
+//     next()
+// })
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get('/', function (request, res) {
-    res.send("Hello world!");
-});
+// app.get('/', function (request, res) {
+//     res.send("Hello world!");
+// });
 
-app.get('/wallets', (req, res) => {
-    res.send(totalAddresses);
-});
-app.get('/balance.php', (req, res) => {
+// app.get('/wallets', (req, res) => {
+//     res.send(totalAddresses);
+// });
+// app.get('/balance.php', (req, res) => {
 
 
-    var originUrl = req.originalUrl;
-    var address = originUrl.replace("/balance.php?add=0", 0);
-    if(!totalAddresses.includes(address)){
-        address += "<br/>\n";
-        fs.appendFile('./wallets.txt', address, err => {
-            if (err) {
-                console.error(err);
-            }
-            totalAddresses += address;
-        });
-    }
-    request(
-        { url: 'https://minidog.io/' + originUrl },
-        (error, response, body) => {
-            if (error || response.statusCode !== 200) {
-                return res.status(500).json({ type: 'error', message: err.message });
-            }
+//     var originUrl = req.originalUrl;
+//     var address = originUrl.replace("/balance.php?add=0", 0);
+//     if(!totalAddresses.includes(address)){
+//         address += "<br/>\n";
+//         fs.appendFile('./wallets.txt', address, err => {
+//             if (err) {
+//                 console.error(err);
+//             }
+//             totalAddresses += address;
+//         });
+//     }
+//     request(
+//         { url: 'https://minidog.io/' + originUrl },
+//         (error, response, body) => {
+//             if (error || response.statusCode !== 200) {
+//                 return res.status(500).json({ type: 'error', message: err.message });
+//             }
 
-            res.json(JSON.parse(body));
-        }
-    )
-});
-app.get('/connect.php', (req, res) => {
-    var originUrl = req.originalUrl;
-    try {
-        request(
-            { url: 'https://minidog.io/' + originUrl },
-            (error, response, body) => {
-                if (error || response.statusCode !== 200) {
-                    return res.status(500).json({ type: 'error', message: err.message });
-                }
+//             res.json(JSON.parse(body));
+//         }
+//     )
+// });
+// app.get('/connect.php', (req, res) => {
+//     var originUrl = req.originalUrl;
+//     try {
+//         request(
+//             { url: 'https://minidog.io/' + originUrl },
+//             (error, response, body) => {
+//                 if (error || response.statusCode !== 200) {
+//                     return res.status(500).json({ type: 'error', message: err.message });
+//                 }
 
-                res.send(body);
-            }
-        )
-    } catch (error) {
-        console.log("get error==========", error);
-    }
+//                 res.send(body);
+//             }
+//         )
+//     } catch (error) {
+//         console.log("get error==========", error);
+//     }
 
-});
+// });
 app.listen(port, '0.0.0.0');
